5000232b7684e61ff44a1a3d0414d60e01c813eb
Saim Amir <saimxamir@gmail.com>
2024-07-12 23:19:10 -0400

    Create temp pages

---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>Properties Page</h1>
+      <Link href='/'>Return to Home Page</Link>
+    </div>
+  );
+};
+
+export default PropertiesPage;
