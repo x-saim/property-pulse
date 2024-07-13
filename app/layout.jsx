1676a64320ba9d9491137beeb96760f583a8d68c
x-saim <saimxamir@gmail.com>
2024-07-13 13:19:22 -0400

    Create navbar, add state for dropdown

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '@/assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -11,7 +12,8 @@ const MainLayout = ({ children }) => {
   return (
     <html lang='en'>
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,16 @@
+'use client';
 import React from 'react';
+import { useRouter, useParams } from 'next/navigation';
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const { id } = useParams();
+
+  return (
+    <div>
+      <div className='text-3xl'>PropertyPage</div>
+    </div>
+  );
 };
 
 export default PropertyPage;
