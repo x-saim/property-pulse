5a053c1e5576d6b58189df080ab7ba7c90c0d865
x-saim <saimxamir@gmail.com>
2024-07-13 19:08:18 -0400

    Update Hero component and Home Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
-
+import Footer from '@/components/Footer';
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
   description: 'Find your dream rental property.',
@@ -14,6 +14,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
