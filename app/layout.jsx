5d7059699176f1799a225ab99fddf1233b1731af
x-saim <saimxamir@gmail.com>
2024-07-17 12:30:48 -0400

    Setup google OAuth and next-auth@4 providers

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '@/assets/styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/authProvider';
 
 export const metadata = {
   title: 'PropertyPulse | Find The Perfect Rental',
@@ -11,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang='en'>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
