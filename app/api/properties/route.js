5186c598ed936dea7d871941ea0a0272f33f17c5
x-saim <saimxamir@gmail.com>
2024-07-17 20:06:10 -0400

    Create POST route for adding new property to db

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from '@/config/db';
 import Property from '@/models/Property';
 import { NextResponse } from 'next/server';
+import { getSessionUser } from '@/utils/getSessionUser';
 
 // GET /api/properties
 /**
@@ -31,5 +32,67 @@ export const GET = async (request) => {
     return NextResponse.json('An error occurred while processing the request', {
       status: 500,
     });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    // Get user ID from session
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return NextResponse('User ID is required.', { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Access form data
+    const formData = await request.formData();
+
+    // Access all values for amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData
+      .getAll('images')
+      .filter((image) => image.name !== '');
+
+    // Create the propertyData object with embedded seller_info
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly.'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      owner: userId,
+      // images,
+    };
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    return NextResponse.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+  } catch (error) {
+    return new Response('Failed to add property', { status: 500 });
   }
 };
